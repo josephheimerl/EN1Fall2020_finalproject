7d4c0b840880bdb47789a5fbeca573116d88a107
josephheimerl <jheimerl208@gmail.com>
2020-12-03 10:52:00 -0700

    Api and base ID

--- src/secret.js
+++ src/secret.js
@@ -16,8 +16,8 @@ var getAPIusername = ""; // fill in with your name (if left blank, assuming usin
 // (2) Hardcode your API key information here
 
 // AIRTABLE SUPPORT
-var airtable_api = "key" + "XXXXXXXXXXX";     // fill in your info
-var airtable_baseid = "app" + "YYYYYYYYYYYY"; // fill in your info
+var airtable_api = "key" + "ndea7TKrKjJSbf";     // fill in your info
+var airtable_baseid = "app" + "o1qN5rMf632X0t"; // fill in your info
 
 // SYSTEMLINK SUPPORT
 var systemlink_api = "XXXXXXXXXXXXXXXXXX"; // fill in your info
